7849c0506f8bda40cff6844cc31389659394bf1d
demo2099 <available3099@gmail.com>
2020-03-08 18:16:59 +0800

    额

--- js/mfjsq.js
+++ js/mfjsq.js
@@ -2,4 +2,4 @@
 author:demo
 群组https://t.me/demo2099
  */
-var body=$response.body.replace(/enable\":0/g,'enable":1 $done({body});
+var body=$response.body.replace(/enable\":0/g,'enable":1'); $done({body});
