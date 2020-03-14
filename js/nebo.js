86240ad8c50da819810664c58f74e47b6b933862
demo2099 <available3099@gmail.com>
2020-03-14 10:18:18 +0800

    nebo内购

---
+++ js/nebo.js
@@ -0,0 +1,11 @@
+/*
+author:demo
+群组https://t.me/demo2099
+ */
+var body=$response.body;body={
+ "originalApplicationVersion": "2.2.4.2936",
+ "dateReceiptCreation": 1582695984000,
+ "inapps": [],
+ "bundleIdentifier": "com.myscript.nebo",
+ "bundleVersion": "2.5.0.5032"
+};body=JSON.stringify(body);$done({body});
