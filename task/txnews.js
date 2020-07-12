853a1d970620ae371f8725434f9b512956da0cc5
demo2099 <61452252+demo2099@users.noreply.github.com>
2020-07-12 13:20:58 +0800

    Update txnews.js

--- task/txnews.js
+++ task/txnews.js
@@ -36,7 +36,7 @@ hostname = api.inews.qq.com
 Cookie获取后，请注释掉Cookie地址。
 #腾讯新闻app签到，根据红鲤鱼与绿鲤鱼与驴修改
 */
-const notify = 0; //开启通知为1，关闭为0
+const notify = 1; //开启通知为1，关闭为0
 const logs = 1; // 日志开关
 const cookieName = '腾讯新闻'
 const signurlKey = 'sy_signurl_txnew17'
