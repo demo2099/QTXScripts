82da2dd45eda249e12682275adcde95c0b5d2087
demo <available3099@gmail.com>
2020-09-22 16:12:45 +0800

    youtubeplus

---
+++ js/youtubeplus.js
@@ -0,0 +1,28 @@
+/*
+author:demo
+群组https://t.me/demo2099
+https://api.cercube.com/v2/user
+ */
+
+var body = $response.body;
+body={
+  "id" : 386641,
+  "updatedAt" : "2020-04-08T07:55:03.413Z",
+  "lastName" : "demo",
+  "firstName" : "mt",
+  "blocked" : false,
+  "roleId" : 1,
+  "didMigrate" : true,
+  "countryCode" : "CN",
+  "customerId" : "cus_H3g6yrkdYeL0Lh",
+  "username" : "demo2099",
+  "subscriptions" : [
+
+  ],
+  "email" : "xxxx@gmail.com"
+};
+
+body = JSON.stringify(obj);
+$done({
+    body
+});
