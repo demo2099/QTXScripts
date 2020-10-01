5c7ecc81b5ee66897cb06ca6c93799f02292ddcb
demo <available3099@gmail.com>
2020-10-01 08:41:18 +0800

    奇热

--- js/qirenol.js
+++ js/qirenol.js
@@ -5,18 +5,7 @@ https://api.cercube.com/v2/user
  */
  var body = $response.body;
 let url=$request.url;
-if($request.url.indexOf("user/info") != -1 || $request.url.indexOf("login/phone_login") != -1){
-body=body.replace(/type\":\"\d/g,'type":"4').replace(/vip_expire\":\"\d+/g,'vip_expire":"5201314007');
-var body = JSON.parse(body); 
-body.data.info.avatar="https://s1.ax1x.com/2020/09/20/w7lOEQ.gif";
-body.data.info.vip=1;
-body.data.info.nickname="呆猫demo";let coin={
-        "new_cash": "5201314.00",
-        "new_coin": "5200"
-      };
-      body.data.info.withdraw_data=coin;
-      body.data.info.cash=5201314.00;
-}else if($request.url.indexOf("user_welfare/add_free_chapter_record") != -1){
+if($request.url.indexOf("user_welfare/add_free_chapter_record") != -1){
 body={
   "status" : "success",
   "code" : 1,
